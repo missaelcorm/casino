3f607a34c1625e59f4c8728e8218061761060a6e
Missael Cortes <missaelcorm@gmail.com>
2025-11-22 15:36:47 -0600

    Fix terraform warning of unsupported attr

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -225,8 +225,8 @@ resource "aws_iam_role_policy" "frontend_task" {
           "secretsmanager:GetSecretValue"
         ]
         Resource = [
-          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project}/${var.environment}/*",
-          "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${var.project}/${var.environment}/*"
+          "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/${var.project}/${var.environment}/*",
+          "arn:aws:secretsmanager:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:secret:${var.project}/${var.environment}/*"
         ]
       }
     ]
